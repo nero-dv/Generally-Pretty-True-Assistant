4a7d3c584cd2b00e08c3782365f51b667e7e056f
nero-dv <louis_noir@protonmail.com>
2023-03-30 21:32:36 -0500

    Resolves #11, #8, #3
    Signed-off-by: nero-dv <louis_noir@protonmail.com>

--- InterfaceUtility.py
+++ InterfaceUtility.py
@@ -1,8 +1,12 @@
 import os
+
 import openai
-from PySide6.QtCore import QObject, Qt, QEvent
-from PySide6.QtWidgets import QMessageBox, QTextEdit, QApplication
-from PySide6.QtGui import QKeySequence, QClipboard, QKeyEvent
+from PySide6.QtCore import QObject, Qt
+from PySide6.QtWidgets import (QDialog, QDialogButtonBox,
+                               QErrorMessage, QFileDialog, QHBoxLayout,
+                               QInputDialog, QLabel, QLineEdit, QListWidget,
+                               QMessageBox, QSizePolicy, QSplitter, QTextEdit,
+                               QVBoxLayout, QWidget)
 
 
 class ChatInput(QTextEdit):
@@ -26,7 +30,7 @@ class ChatModel(QObject):
         messages = [
             {
                 "role": "system",
-                "content": "You help me learn how to write code.",
+                "content": "You're an ML model designed to answer questions.",
             }
         ]
         for i in range(num_inputs):
@@ -35,14 +39,23 @@ class ChatModel(QObject):
                 messages.append({"role": "assistant", "content": assistant_response[i]})
         return messages
 
-    def submit_text(self, input_text, assistant_response, model="gpt-3.5-turbo"):
-        try:
-            if os.environ.get("OPENAI_API_KEY"):
-                OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-            else:
-                OPENAI_API_KEY = self.read_api_key()
-        except:
-            print("No API key found. Please set one in the menu.")
+    def submit_text(self, OPENAI_API_KEY, input_text, assistant_response, model="gpt-3.5-turbo"):
+        if OPENAI_API_KEY is None:
+            print("No API key. Please set one in the menu.")
+            error_box = QMessageBox()
+            error_box.setIcon(QMessageBox.Critical)
+            error_box.setText("Invalid API key. Please set one in the menu.")
+            error_box.setWindowTitle("Invalid API key")
+            error_box.setStandardButtons(QMessageBox.Ok)
+            error_box.exec()
+            return
+        #     # if os.environ.get("OPENAI_API_KEY"):
+        #     #     #OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+        #     #     print("API key found in environment variable OPENAI_API_KEY")
+        #     print("No API key found. Please set one in the menu.")
+
+        #     #print(OPENAI_API_KEY)
+            
         openai.api_key = OPENAI_API_KEY
         try:
             response = openai.ChatCompletion.create(
@@ -63,3 +76,141 @@ class ChatModel(QObject):
             return f'Request failed: {e}'
         return response
     
+class ApiWindow(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("API Key Manager")
+        self.resize(500, 300)
+        self.list_widget = QListWidget()
+        self.list_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        if os.environ.get("OPENAI_API_KEY") and os.environ.get("OPENAI_API_KEY") != "":
+            if self.list_widget.findItems(
+                str(os.environ.get("OPENAI_API_KEY")), Qt.MatchExactly
+            ):
+                pass
+            self.list_widget.addItem(str(os.environ.get("OPENAI_API_KEY")))
+        elif os.path.isfile("api_key.aik"):
+            with open("api_key.txt", "r") as f:
+                key = f.read()
+            if self.list_widget.findItems(key, Qt.MatchExactly):
+                pass
+            self.list_widget.addItem(key)
+        elif self.key:
+            if self.list_widget.findItems(self.key, Qt.MatchExactly):
+                pass
+            self.list_widget.addItem(self.key)
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self.handle_accept)
+        button_box.rejected.connect(self.reject)
+        second_button_box = QDialogButtonBox()
+        add_key = second_button_box.addButton("Add Key", QDialogButtonBox.ActionRole)
+        add_key.clicked.connect(self.handle_add)
+        remove_key = second_button_box.addButton(
+            "Remove Key", QDialogButtonBox.ActionRole
+        )
+        remove_key.clicked.connect(self.remove_selected)
+        browse = second_button_box.addButton("Browse", QDialogButtonBox.ActionRole)
+        browse.clicked.connect(self.open)
+        layout = QVBoxLayout()
+        splitter = QSplitter(Qt.Vertical)
+        label = QLabel("Enter your API key:")
+        layout.addWidget(label)
+        self.text_edit = QLineEdit()
+        layout.addWidget(self.list_widget)
+        layout.addWidget(self.text_edit)
+        splitter.addWidget(second_button_box)
+        splitter.addWidget(button_box)
+        layout.addWidget(splitter)
+        self.list_widget.mouseDoubleClickEvent = self.handle_accept
+        self.key = None
+        self.setLayout(layout)
+        self.exec()
+
+    def validate_input(self):
+        if (
+            self.text_edit == "" or self.text_edit.text() == ""
+        ) and self.none_selected():
+            return False
+        return True
+    
+    def handle_double_click(self):
+        
+        self.handle_accept()
+
+    def handle_accept(self, event=None):
+        if self.validate_input():
+            try:
+                #print(self.get_selected_key())
+                self.key = self.get_selected_key()
+                self.key = str(self.key)
+                self.done(QDialog.Accepted)
+                self.result()
+                
+            except Exception as e:
+                print(e)
+        elif self.none_selected():
+            error_dialog = QErrorMessage()
+            error_dialog.showMessage("No API key selected")
+            error_dialog.setWindowTitle("Error")
+            
+    def result(self):
+        return self.key
+
+    def get_selected_key(self):
+        self.selected_key = self.list_widget.selectedItems()[0].text()
+        return self.selected_key
+
+    def handle_add(self):
+        if len(self.text_edit.text()) > 0 and " " not in self.text_edit.text():
+            self.add_to_list(str(self.text_edit.text()))
+            self.text_edit.clear()
+
+    def open(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file_name = QFileDialog.getOpenFileName(
+            None,
+            "QFileDialog.getOpenFileName()",
+            ".aik",
+            "AI Key Files (*.aik);;All Files(*)",
+            options=options,
+        )
+
+        if file_name[0]:
+            with open(file_name[0], "r") as file:
+                for line in file.readlines():
+                    key = line.strip()
+                    if self.list_widget.findItems(key, Qt.MatchExactly):
+                        error_dialog = QErrorMessage()
+                        error_dialog.showMessage("Key already exists in the list.")
+                        error_dialog.setWindowTitle("Error")
+                        error_dialog.exec()
+                    else:
+                        self.add_to_list(key)
+
+    def add_to_list(self, text):
+        self.list_widget.addItem(text)
+
+    def remove_selected(self):
+        selected = self.list_widget.selectedItems()
+        for item in selected:
+            self.list_widget.takeItem(self.list_widget.row(item))
+
+    def none_selected(self):
+        return self.list_widget.selectedItems() == []
+
+
+class SmallKeyManager(QWidget):
+
+    def set_key(self):
+        api_key, ok = QInputDialog.getText(
+            self, "QInputDialog.getText()", "API Key:", QLineEdit.Normal  # type: ignore
+        )
+        if ok and api_key:
+            OPENAI_API_KEY = api_key
+            return OPENAI_API_KEY
+
+    def save_api_key(self, key):
+        with open("api_key.aik", "w") as file:
+            file.write(key)
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import sys
 import json
+import sys
+import os
 
+import markdown2
 import tiktoken
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QAction, QFont, QFontDatabase, QIcon, QTextCursor
-from PySide6.QtWidgets import (QApplication, QComboBox, QFileDialog,
-                               QHBoxLayout, QInputDialog, QLabel, QLineEdit,
-                               QMainWindow, QMenu, QMenuBar, QPushButton,
-                               QSplitter, QTabWidget, QTextEdit, QVBoxLayout,
-                               QWidget)
+from PySide6.QtGui import QAction, QFont, QFontDatabase, QIcon, QTextCursor, QTextOption
+from PySide6.QtWidgets import (QApplication, QComboBox,
+                               QFileDialog, QHBoxLayout, QLabel, QMainWindow,
+                               QMenu, QMenuBar, QPushButton, QSplitter,
+                               QTabWidget, QTextBrowser, QTextEdit,
+                               QVBoxLayout, QWidget)
 
-from InterfaceUtility import ChatInput, ChatModel
+from InterfaceUtility import ChatInput, ChatModel, ApiWindow
 
 
 class MainWindow(QMainWindow):
@@ -20,6 +22,7 @@ class MainWindow(QMainWindow):
         self.assistant_response = []
         self.init_ui()
         self.setWindowIcon(QIcon("img/icon.ico"))
+        self.OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", None)
 
     def init_ui(self):
         fontdb = QFontDatabase()
@@ -83,13 +86,13 @@ class MainWindow(QMainWindow):
 
         self.splitter = QSplitter(Qt.Vertical)  # type: ignore
 
-        self.chat = QTextEdit()
+        self.chat = QTextBrowser()
         self.chat.setFont(self.response_font)
         self.chat.setFontPointSize(11)
         self.chat.setPlaceholderText("Your assistant's response will appear here")
         self.chat.setAcceptRichText(True)
+        self.chat.setWordWrapMode(QTextOption.WrapAtWordBoundaryOrAnywhere)
         self.chat.setReadOnly(True)
-        self.chat.toMarkdown()
         self.splitter.addWidget(self.chat)
 
         self.size_button_widget = QWidget()
@@ -203,12 +206,13 @@ class MainWindow(QMainWindow):
         self.export_history_action = add_menu_action(
             file_menu, "&Export History", "Ctrl+E", self.export_history
         )
-        self.set_api_action = add_menu_action(
-            file_menu, "&Set API Key", "Ctrl+K", self.set_api_key
-        )
-        # self.api_key_option = add_menu_action(
-        #     file_menu, "&API Key Manager", "Ctrl+M", self.api_key_manager
+        # self.set_api_action = add_menu_action(
+        #     file_menu, "&Set API Key", "Ctrl+K", self.set_api_key
         # )
+        self.api_key_option = add_menu_action(
+            file_menu, "&API Key Manager", "Ctrl+M", self.api_key_manager
+        )
+        
         self.quit_action = add_menu_action(file_menu, "&Quit", "Ctrl+Q", self.close)
 
         # Menu two
@@ -230,7 +234,7 @@ class MainWindow(QMainWindow):
 
     def save_chat(self):
         file_path, _ = QFileDialog.getSaveFileName(
-            self, "Save Chat", "", "MD Files (*.MD);;All Files (*)"
+            self, "Save Chat", ".MD", "MD Files (*.MD);;All Files (*)"
         )
 
         if file_path:
@@ -239,37 +243,38 @@ class MainWindow(QMainWindow):
 
     def export_history(self):
         file_path, _ = QFileDialog.getSaveFileName(
-            self, "Export Raw History", "", "JSON Files (*.json);;All Files (*)"
+            self, "Export Raw History", ".json", "JSON Files (*.json);;All Files (*)"
         )
         if file_path:
             with open(file_path, "w") as file:
                 file.write(self.history.toPlainText())
 
-    def set_api_key(self):
-        # Use a dialog to get the API key
-        api_key, ok = QInputDialog.getText(
-            self, "QInputDialog.getText()", "API Key:", QLineEdit.Normal  # type: ignore
-        )
-        if ok and api_key:
-            self.save_api_key(api_key)
+    # def set_api_key(self):
+    #     # Use a dialog to get the API key
+    #     api_key, ok = QInputDialog.getText(
+    #         self, "QInputDialog.getText()", "API Key:", QLineEdit.Normal  # type: ignore
+    #     )
 
-    def save_api_key(self, api_key):
-        with open("api_key.txt", "w") as file:
-            file.write(api_key)
-        return api_key
 
-    def read_api_key(self):
-        try:
-            with open("api_key.txt", "r") as file:
-                return file.read()
-        except FileNotFoundError:
-            # Use a dialog to get the API key
-            self.set_api_key()
+    # def save_api_key(self, api_key):
+    #     with open("api_key.aik", "w") as file:
+    #         file.write(api_key)
+    #         self.OPENAI_API_KEY = api_key
+    #     return api_key, self.OPENAI_API_KEY
+
+    # def read_api_key(self):
+    #     try:
+    #         with open("api_key.aik", "r") as file:
+    #             return file.read()
+    #     except FileNotFoundError:
+    #         # Use a dialog to get the API key
+    #         self.set_api_key()
 
     def api_key_manager(self):
-        # TODO: Add a dialog to manage API keys along with a way to select which one to use and a way to delete them. API key utilization should be tracked and shown in the manager.
-        pass
-
+        get_key = ApiWindow(self).key
+        self.OPENAI_API_KEY = get_key
+        
+        
     def copy_text(self):
         self.input_text_edit.copy()
 
@@ -301,30 +306,48 @@ class MainWindow(QMainWindow):
         return f"Token count: {num_tokens}\tWord count: {len(text.split())},\tCharacter count: {len(text)}"
     
     def submit_text(self):
+        if self.input_text_edit.toPlainText() == "" or self.input_text_edit.toPlainText() == " ":
+            return
+        if self.OPENAI_API_KEY == "" or self.OPENAI_API_KEY is None:
+            run_api_key_manager = ApiWindow()
+            self.OPENAI_API_KEY = run_api_key_manager.result()
+
         self.input_text_list.append(self.input_text_edit.toPlainText())
         self.num_contexts = self.context_choice.currentIndex() + 1
-        response = ChatModel().submit_text(
+        
+        response = ChatModel().submit_text(self.OPENAI_API_KEY,
             self.input_text_list[-1 * self.num_contexts :],
             self.assistant_response[-1 * (self.num_contexts - 1) :],
         )
-        self.assistant_response.append(response.choices[0].message.content)
-        token_usage = self.token_count(response)
-        self.parse_response(response, token_usage)
-        self.history.append(json.dumps(response, indent=4))
+        if response:
+            self.assistant_response.append(response.choices[0].message.content)
+            token_usage = self.token_count(response)
+            self.parse_response(response, token_usage)
+            self.history.append(json.dumps(response, indent=4))
+        elif self.OPENAI_API_KEY == "" or self.OPENAI_API_KEY is None:
+            self.assistant_response.append("API Key was not set.")
+            self.parse_response(response, "")
+            return
+        else:
+            self.assistant_response.append("No response from OpenAI.")
+            self.parse_response("", "")
         self.input_text_edit.clear()
+        return
 
     def parse_response(self, response, token_usage):
         print(self.input_text_list[-1])
         print(self.assistant_response[-1])
-        self.chat.append(f"{self.input_text_list[-1]}")
-        self.chat.append("\n\t" + "-" * 60 + "\n")
-        self.chat.append(f"{self.assistant_response[-1]}")
+        markdown = self.chat.toMarkdown() if self.chat.toMarkdown() else ""
+        markdown = markdown + (f"####{self.input_text_list[-1]}\n")
+        markdown = markdown + ("___" + "\n<br>\n")
+        markdown = markdown + (f"{self.assistant_response[-1]}\n")
+        markdown = markdown + ("\n<br>\n")
+        markdown = markdown + (token_usage)
+        markdown = markdown + ("___" + "\n<br>\n<br>\n")
+        html_text = markdown2.markdown(markdown)
+        self.chat.setHtml(html_text)
         self.chat.moveCursor(QTextCursor.End)
-        self.chat.append("\n")
-        self.chat.append(token_usage)
-        self.chat.append("\t" + "-" * 60 + "\n")
-        self.chat.toMarkdown()
-
+    
     def token_count(self, response):
         # Get usage information from the API response
         completion_tokens = response.usage.completion_tokens
